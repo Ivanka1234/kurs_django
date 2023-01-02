947a6de3711211f3b587c9b9b898bb5916622486
Ivanka1234 <gerasymyuk_az15@nuwm.edu.ua>
2023-01-02 14:44:45 +0200

    urls

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    print(request)
+    #print(request)
     return HttpResponse('Hello world')
 
+def test(request):
+    #print(request)
+    return HttpResponse('<h1> Тестова сторінка </h1>')
