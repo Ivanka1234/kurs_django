33a84938660d3ae5a55f35ac265a5ea883851c18
Ivanka1234 <gerasymyuk_az15@nuwm.edu.ua>
2023-01-04 16:04:06 +0200

    zvjazok adminka

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -7,6 +7,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name="оновлено")
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="фото", blank=True)
     is_published = models.BooleanField(default=True, verbose_name="Опубліковано?")
+    category=models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категорія")
 
 
     def __str__(self):
@@ -16,3 +17,13 @@ class News(models.Model):
         verbose_name="Новина"
         verbose_name_plural = "Новини"
         ordering=['-created_at']
+
+class Category(models.Model):
+    title= models.CharField(max_length=150, db_index=True, verbose_name="Назва категорії")
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name="Категорія"
+        verbose_name_plural = "Категорії"
+        ordering=['title']
